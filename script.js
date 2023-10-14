8124d9af29e9efb9608453f9bc72a1d748201e14
justin <justindachille@gmail.com>
2023-10-14 00:58:29 -0700

    reset button clears

--- script.js
+++ script.js
@@ -77,8 +77,9 @@ window.addEventListener('load', () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = "white";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
+      convertToAscii();
     };
-    
+
     window.convertToAscii = () => {
         let asciiArt = '';
         const asciiColumns = 26;
