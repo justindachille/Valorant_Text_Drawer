6ce0e20ae10ee2fa6f5a75c2d7620b194fdc0572
justin <justindachille@gmail.com>
2023-10-14 00:43:02 -0700

    add erasing

--- script.js
+++ script.js
@@ -4,87 +4,95 @@ window.addEventListener('load', () => {
     const ctx = canvas.getContext('2d');
     let penSize = 40;
     let drawing = false;
+    let erasing = false;
     let livePreviewInterval;
+    
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    canvas.addEventListener('mousedown', () => {
-        drawing = true;
+    
+    canvas.addEventListener('mousedown', (e) => {
+        if (e.button === 0) {
+            drawing = true;
+        } else if (e.button === 2) {
+            erasing = true;
+        }
         livePreviewInterval = setInterval(convertToAscii, 100);
-      });
-      
-      canvas.addEventListener('mouseup', () => {
+    });
+    
+    canvas.addEventListener('mouseup', () => {
         drawing = false;
+        erasing = false;
         ctx.beginPath();
         clearInterval(livePreviewInterval);
-      });
+    });
+    
+    canvas.addEventListener('contextmenu', (e) => {
+        e.preventDefault();
+    });
+    
     penSizeSlider.addEventListener('input', function() {
         penSize = this.value;
     });
+    
+    window.addEventListener('mouseup', () => {
+        drawing = false;
+        erasing = false;
+        ctx.beginPath();
+        clearInterval(livePreviewInterval);
+    });
+    
+    canvas.addEventListener('mousemove', draw);
+    
+    function draw(event) {
+        if (!drawing && !erasing) return;
+    
+        ctx.lineWidth = penSize;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = erasing ? 'white' : 'black';
+    
+        ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    }
 
-
-  
     function setCanvasSize() {
       const maxWidth = window.innerWidth * 0.5;
-      const aspectRatio = 26 / 12; // Width to Height ratio of ASCII art output
+      const aspectRatio = 26 / 12;
   
-      // Calculate canvas dimensions
       const canvasWidth = Math.floor(maxWidth);
       const canvasHeight = Math.floor(canvasWidth / aspectRatio);
     
       canvas.width = canvasWidth;
       canvas.height = canvasHeight;
   
-      // Initialize canvas with white background
       ctx.fillStyle = "white";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
     setCanvasSize();
 
-    // Resize canvas when window resizes (optional)
     window.addEventListener('resize', setCanvasSize);
-
-    window.addEventListener('mouseup', () => {
-        drawing = false;
-        ctx.beginPath();
-        clearInterval(livePreviewInterval);
-    });
-  
-    canvas.addEventListener('mousemove', draw);
 
     window.resetCanvas = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = "white";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
     };
-
-    function draw(event) {
-        if (!drawing) return;
-        
-        ctx.lineWidth = penSize;  // Use dynamic pen size
-        ctx.lineCap = 'round';
-        ctx.strokeStyle = 'black';
-      
-        ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
-      }
-  
       window.convertToAscii = () => {
         let asciiArt = '';
         const asciiColumns = 26;
         const asciiRows = 12;
       
-        // Calculate step sizes based on canvas dimensions and desired ASCII dimensions (26x12)
-        const xStep = canvas.width / (asciiColumns * 2);  // 2 because each character will represent a half box either at the top or bottom
-        const yStep = canvas.height / (asciiRows * 2);    // 2 for the same reason as xStep
+        const xStep = canvas.width / (asciiColumns * 2);
+        const yStep = canvas.height / (asciiRows * 2);
       
         for (let y = 0; y < asciiRows * 2; y += 2) {
           for (let x = 0; x < asciiColumns * 2; x += 2) {
       
             let topHalf = 0;
             let bottomHalf = 0;
+            let leftHalf = 0;
+            let rightHalf = 0;
       
             for (let subY = 0; subY < 2; subY++) {
               for (let subX = 0; subX < 2; subX++) {
@@ -92,7 +100,9 @@ window.addEventListener('load', () => {
                 const grayscale = (pixel[0] + pixel[1] + pixel[2]) / 3;
                 if (grayscale < 128) {
                   if (subY === 0) topHalf++;
-                  else bottomHalf++;
+                  if (subY === 1) bottomHalf++;
+                  if (subX === 0) leftHalf++;
+                  if (subX === 1) rightHalf++;
                 }
               }
             }
@@ -103,6 +113,10 @@ window.addEventListener('load', () => {
               asciiArt += '▀';
             } else if (bottomHalf === 2) {
               asciiArt += '▄';
+            } else if (leftHalf === 2) {
+              asciiArt += '▌';
+            } else if (rightHalf === 2) {
+              asciiArt += '▐';
             } else {
               asciiArt += '░';
             }
